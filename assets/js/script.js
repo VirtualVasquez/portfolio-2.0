c084f7dff8f88f6d6871f79c2819469986f9961b
VirtualVasquez <mxv647@med.miami.edu>
2020-08-17 20:17:21 -0400

    slick.js carousel implemented, base level.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
-//function to load About Me
-//function to load previous projects
-//function to load skill set
-//function to load contact information
 
+$('.carousel').slick({
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  dots:false,
+  arrows:false,
+  draggable: false
+});
 
-//nav event listeners
-document.addEventListener("click", function(event){
-    if(event.target.matches("#about-me")){
-        document.body.style.backgroundColor = "red";
-    }
-    if(event.target.matches("#projects")){
-        document.body.style.backgroundColor = "blue";
-    } 
-    if(event.target.matches("#skill-set")){
-        document.body.style.backgroundColor = "green";
-    } 
-    if(event.target.matches("#contact")){
-        document.body.style.backgroundColor = "yellow";
-    } 
-})
+$('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.carousel').slick('slickGoTo', slideno -1);
+});
+
