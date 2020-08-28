bd2d9b2143094d8276fa748e9ae7110df4fcf93b
VirtualVasquez <mxv647@med.miami.edu>
2020-08-28 19:17:57 -0400

    trying to animate intro, might need a promise

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,42 @@ let projectsPage = document.getElementById("projects");
 let contactPage = document.getElementById("contact");
 let badge = document.getElementById("badge");
 
+//delayLoop function and (modified) display function courtesy of Travis Horn | https://travishorn.com/delaying-foreach-iterations-2ebd4b29ad30#:~:text=Now%20you%20can%20use%20delayLoop,to%20delay%20between%20each%20iteration.
+const delayLoop = (fn, delay) => {
+  return (x, i) => {
+    setTimeout(() => {
+      fn(x);
+    }, i * delay);
+  };
+};
+
+function typeWriter(target, text, speed){
+  const output = document.getElementById(target);
+  let arr = text.split("");
+  const display = s => output.innerHTML += s;
+  arr.forEach(delayLoop(display, speed))
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  
+  typeWriter("t1", "Web Developer", 50)
+  typeWriter("t2", "Problem Solver", 50)
+  typeWriter("t3", "Creative Thinker", 50)
+  typeWriter("t4", "Making cool creations with clean code", 20)
+  
+
+})
+//   p.then((message) = > {
+//     console.log("This is in the then" +  message)
+//   }).catch((message) => {
+//     console.log("this is in the catch" + message)
+//   })
+
+// });
+
+
 
 $(function () {
   // Grab the template script
